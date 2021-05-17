9119054225e836d93b0ed665dbb4ec2d7ccbc8bc
Marinos <you@example.com>
2021-05-17 19:09:06 -0400

    clean up fixes

--- clean_up_figures.py
+++ clean_up_figures.py
@@ -1,15 +1,16 @@
+""" Clean up figures """
 import subprocess
 import os
 
 print("Cleaning up benchmark results and figures...")
 
-os.chdir("~/musical-guide")
+os.chdir("/home/selest/musical-guide")
 
 rm_cmds = [
-    "rm -rf Fig_*/*.pdf",
-    "rm -rf Fig_5/*/bench_outputs/*",
-    "rm -rf Table_*/table_*.txt"
-    "rm -rf Table_*/bench_outputs/*"
+    "sudo rm -rf Fig_*/*.pdf",
+    "sudo rm -rf Fig_5/*/bench_outputs/*",
+    "sudo rm -rf Table_*/table_*.txt"
+    "sudo rm -rf Table_*/bench_outputs/*"
 ]
 
 for rm_cmd in rm_cmds:
