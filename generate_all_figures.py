c0d9995502f7a5365891989a203ec5747287823e
Marinos <you@example.com>
2021-05-17 15:49:23 -0400

    run gen_all_figs with sudo

--- generate_all_figures.py
+++ generate_all_figures.py
@@ -5,37 +5,37 @@ import subprocess
 
 print("Generating Figure 3...")
 os.chdir("Fig_3")
-cmd = "python3 gen_fig3.py"
+cmd = "sudo python3 gen_fig3.py"
 subprocess.run(cmd.split())
 
 print("Generating Figure 4...")
 os.chdir("Fig_4")
-cmd = "python3 gen_fig4.py"
+cmd = "sudo python3 gen_fig4.py"
 subprocess.run(cmd.split())
 
 print("Generating Figure 5...")
 os.chdir("Fig_5")
-cmd = "python3 gen_fig5.py"
+cmd = "sudo python3 gen_fig5.py"
 subprocess.run(cmd.split())
 
 print("Generating Table 1...")
 os.chdir("Table_1")
-cmd = "python3 gen_tab1.py"
+cmd = "sudo python3 gen_tab1.py"
 subprocess.run(cmd.split())
 
 print("Generating Table 2...")
 os.chdir("Table_2")
-cmd = "python3 gen_tab2.py"
+cmd = "sudo python3 gen_tab2.py"
 subprocess.run(cmd.split())
 
 print("Generating Table 3...")
 os.chdir("Table_3")
-cmd = "python3 gen_tab3.py"
+cmd = "sudo python3 gen_tab3.py"
 subprocess.run(cmd.split())
 
 print("Generating Table 4...")
 os.chdir("Table_4")
-cmd = "python3 gen_tab4.py"
+cmd = "sudo python3 gen_tab4.py"
 subprocess.run(cmd.split())
 
 print("All figures and tables should be generated in their respective directories.")
