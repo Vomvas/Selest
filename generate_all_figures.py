a779c818036061ff2d49f81a2973ac41fd652155
musical-guide <>
2021-05-17 16:22:17 -0400

    naming fixes

--- generate_all_figures.py
+++ generate_all_figures.py
@@ -1,5 +1,5 @@
-""" TL;DR script to generate all figures and tables for replicability purposes """
 #!/usr/bin/python3
+""" TL;DR script to generate all figures and tables for replicability purposes """
 import os
 import subprocess
 
@@ -7,35 +7,42 @@ print("Generating Figure 3...")
 os.chdir("Fig_3")
 cmd = "sudo python3 gen_fig3.py"
 subprocess.run(cmd.split())
+os.chdir("..")
 
 print("Generating Figure 4...")
 os.chdir("Fig_4")
 cmd = "sudo python3 gen_fig4.py"
 subprocess.run(cmd.split())
+os.chdir("..")
 
 print("Generating Figure 5...")
 os.chdir("Fig_5")
 cmd = "sudo python3 gen_fig5.py"
 subprocess.run(cmd.split())
+os.chdir("..")
 
 print("Generating Table 1...")
 os.chdir("Table_1")
 cmd = "sudo python3 gen_tab1.py"
 subprocess.run(cmd.split())
+os.chdir("..")
 
 print("Generating Table 2...")
 os.chdir("Table_2")
 cmd = "sudo python3 gen_tab2.py"
 subprocess.run(cmd.split())
+os.chdir("..")
 
 print("Generating Table 3...")
 os.chdir("Table_3")
 cmd = "sudo python3 gen_tab3.py"
 subprocess.run(cmd.split())
+os.chdir("..")
 
 print("Generating Table 4...")
 os.chdir("Table_4")
 cmd = "sudo python3 gen_tab4.py"
 subprocess.run(cmd.split())
+os.chdir("..")
 
 print("All figures and tables should be generated in their respective directories.")
