a4759df509e362c5820376b6b16daa6ea9228ef6
musical-guide <>
2021-05-17 15:33:24 -0400

    table generation config

--- Table_3/gen_table3.py
+++ Table_3/gen_table3.py
@@ -1,8 +1,12 @@
-table2_str = "Note: Tables 2 and 3 of the initial submission were inadvertently identical. This will be fixed" \
-             "for the camera ready submission." \
-             "The results in table 2 (and 3) depend directly on the results of table 4." \
-             "In order to avoid redundant" \
-             "experiments, table 2 (and 3) is incorporated in table 4 for the purposes of the replicability label." \
-             "Please find it concatenated in table 4."
+table2_str = "Note: Tables 2 and 3 of the initial submission were inadvertently identical. This will be fixed\n" \
+             "for the camera ready submission.\n\n" \
+             "The results in table 2 (and 3) depend directly on the results of table 4.\n" \
+             "In order to avoid redundant " \
+             "experiments, table 2 (and 3) is incorporated in table 4 for the purposes of the replicability label.\n" \
+             "Please find it concatenated in table 4 (last 4 columns)."
 
 print(table2_str)
+
+with open("table_3.txt", "w") as f:
+    f.write(table2_str)
+
