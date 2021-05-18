12b382cc6683899165ccc7dc1e3798419453e1c2
Marinos <you@example.com>
2021-05-17 20:04:53 -0400

    rebuild mpspdz

--- clean_up_figures.sh
+++ clean_up_figures.sh
@@ -2,6 +2,8 @@
 
 echo "Cleaning up benchmark results and figures..."
 
+cd /home/selest/musical-guide
+
 rm -rf Fig_*/*.pdf
 rm -rf Fig_5/*/bench_outputs/*
 rm -rf Table_*/table_*.txt
---
+++ rebuild_mpspdz.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /home/selest/musical-guide/MP_SPDZ_online
+
+make clean
+
+make -j 6 mascot shamir sy lowgear-party.x cowgear-party.x semi-party.x hemi-party.x ps-rep-ring-party.x rep-field yao
+
